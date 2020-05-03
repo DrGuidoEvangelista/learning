b785692d24c52ad4bc20ff1db5dccf1979e6ce5e
DrGuidoEvangelista <62661697+DrGuidoEvangelista@users.noreply.github.com>
2020-05-03 17:08:01 +0200

    Update R_histograms.r

--- R_histograms.r
+++ R_histograms.r
@@ -6,7 +6,6 @@ hist(iris$Sepal.Lenght) #works only in RStudio
 #histograms of group 
 #put graphs in 3 row and 1 colum
 par(mfrow= c(3,1) )     #c is a function for concatenation (3 is the number of rows and 1 column), par means parameter,
-
 hist(iris$Petal.Width  [iris$Species =='setosa'],    #iris$Species is to use only the setosa row
 xlim = c( 0,3), 
 breaks= 9,
@@ -28,8 +27,33 @@ main = 'Petal Width for Setosa',
 xlab = "", #no x lable 
 col= "red" )
 
+#let's do the same thing for 'versicolor'
+hist(iris$Petal.Width   [iris$Species =='versicolor'],
+xlim= c(0,3),
+breaks= 9,
+xlab= "",
+col= 'purple',
+main = "Petal width for Versicolor")
+
+#let's do virginica too
+hist(iris$Petal.Width   [iris$Species =='virginica'],
+xlab= "",
+col= "yellow",
+breaks= 9,
+main = "Petal Width for Virginica",
+xlim= c(0,3) )
 
 
+# Let's see the result of our analysis all together #
+
+
+par(mfrow= c(3,1) )     #c is a function for concatenation (3 is the number of rows and 1 column), par means parameter,
+hist(iris$Petal.Width  [iris$Species =='setosa'],    #iris$Species is to use only the setosa row
+xlim = c( 0,3), 
+breaks= 9,
+main = 'Petal Width for Setosa',
+xlab = "", #no x lable 
+col= "red" )
 
 #let's do the same thing for 'versicolor'
 hist(iris$Petal.Width   [iris$Species =='versicolor'],
@@ -46,6 +70,21 @@ col= "yellow",
 breaks= 9,
 main = "Petal Width for Virginica",
 xlim= c(0,3) )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
